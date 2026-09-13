79e2269bd89c512e40eb2adcbcbdfe0efb40c6d3
mdzeeshan14160 <mdzeeshan14160@gmail.com>
2026-09-13 17:46:38 +0530

    Complete Bank Account System with encapsulation

--- Bank-Account-System/Bank.java
+++ Bank-Account-System/Bank.java
@@ -1,43 +1,75 @@
-
 import java.util.Scanner;
 
 class BankAccount {
 
-    int Account_Number;
-    String Name;
-    int Balance;
+    private  int Account_Number;
+    private  String Name;
+    private int Balance;
+    public int Accountnum(){
+        return Account_Number;
+    }
+    public String getname(){
+        return Name;
+    } 
+    public int getbalance(){
+        return Balance;
+    }
+    void setacc(int n){
+        Account_Number=n;
+    }
+    void setname(String n){
+        Name=n;
+    }
+    void setbal(int n){
+        Balance=n;
+    }
 }
 
 class AccOpr extends BankAccount {
 
     void CreateAcc(int Accno, String Name, int Balance) {
-        this.Account_Number = Accno;
-        this.Name = Name;
-        this.Balance = Balance;
+       setacc(Accno);
+       setname(Name);
+       setbal(Balance);
     }
 
     int deposit(int n) {
-        Balance += n;
-        return Balance;
+        if (n <= 0) {
+            System.out.println("Amount Must Be Greater Than 0");
+            return -1;
+        } else {
+           int temp = getbalance()+n;
+           setbal(temp);
+        }
+        return getbalance();
     }
 
     int withdraw(int n) {
-        if (Balance >= n) {
-            Balance -= n;
+         if(n<=0){
+            System.out.println("Amount Must Be Greater Than 0");
+            return -1;
         }
-        return Balance;
+        else if (getbalance() >= n) {
+            int temp=getbalance()-n;
+            setbal(temp);
+        }
+        else{
+            System.out.println("Insufficient Balance");
+            return -1;
+        }
+        return getbalance();
     }
 
     int checkBal() {
-        return Balance;
+       return getbalance();
     }
 
     void DisplayAcc() {
-        System.out.println("===> Welcome To Our Bank <===");
+        System.out.println("===> ACCOUNT DETAILS <===");
         System.out.println("");
-        System.out.println("=> Account Number : " + Account_Number);
-        System.out.println("=> Account Holder Name : " + Name);
-        System.out.println("=> Account Balance : " + Balance);
+        System.out.println("=> Account Number : " + Accountnum());
+        System.out.println("=> Account Holder Name : " + getname());
+        System.out.println("=> Account Balance : " + getbalance());
         System.out.println("");
     }
 }
@@ -48,46 +80,61 @@ class Bank {
         Scanner sc = new Scanner(System.in);
         AccOpr user1 = new AccOpr();
         boolean bankrun = true;
+		System.out.println("===> Welcome To Our Bank <===");
+        System.out.println("Please Create An Account First");
+		 System.out.print("=> Enter Account Number : ");
+			int acc = sc.nextInt();
+			sc.nextLine();
+			System.out.print("=> Enter Holder Name : ");
+			String name = sc.nextLine();
+			System.out.print("=> Minimum ₹500 Deposit Required to Open an Account : ");
+			boolean min_dep=true;
+			int bal = sc.nextInt();
+			while(min_dep){
+				if(bal<500){
+				System.out.println("=> Minimum ₹500 Deposit Required : ");
+				bal=sc.nextInt();
+				}else{
+					min_dep=false;
+                    System.out.println("=> "+bal+" Deposited Successfully");
+				}
+			}
+			user1.CreateAcc(acc, name, bal);
+			System.out.println();
         while (bankrun) {
-            System.out.println("===> Welcome To Our Bank <===");
-            System.out.println("1. Create Account");
-            System.out.println("2. Deposit");
-            System.out.println("3. Withdraw");
-            System.out.println("4. Check Balance");
-            System.out.println("5. Display Account");
-            System.out.println("6. Exit");
+            System.out.println("1. Deposit");
+            System.out.println("2. Withdraw");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Display Account");
+            System.out.println("5. Exit");
             System.out.println();
             System.out.print("=> Enter your choice: ");
             int n = sc.nextInt();
             switch (n) {
                 case 1 -> {
-                    System.out.print("=> Enter Account Number : ");
-                    int acc = sc.nextInt();
-                    sc.nextLine();
-                    System.out.print("=> Enter Holder Name : ");
-                    String name = sc.nextLine();
-                    System.out.print("=> Enter Balance : ");
-                    int bal = sc.nextInt();
-                    user1.CreateAcc(acc, name, bal);
-                }
-                case 2 -> {
                     System.out.print("=> Enter Deposit Money : ");
                     int money = sc.nextInt();
-                    user1.deposit(money);
+                    int result = user1.deposit(money);
+                    if (result != -1) {
+                        System.out.println("=> "+money+" Deposited Successfully");
+                    }
                 }
-                case 3 -> {
+                case 2 -> {
                     System.out.print("=> Enter Withdraw Money : ");
                     int money = sc.nextInt();
-                    user1.withdraw(money);
+                    int result = user1.withdraw(money);
+                    if (result != -1) {
+                        System.out.println("=> "+money+" Withdraw Successfully");
+                    }
                 }
-                case 4 -> {
+                case  3-> {
                     System.out.println("Balance : " + user1.checkBal());
                 }
-                case 5 -> {
+                case 4 -> {
                     user1.DisplayAcc();
                 }
-                case 6 -> {
-                    System.out.print("=> Thank You (^.^)");
+                case 5 -> {
+                    System.out.print("=> Thank You (^.^) Visit Again");
                     bankrun = false;
                 }
                 default ->
---
+++ Bank-Account-System/Bankjava.java
@@ -0,0 +1,9 @@
+public class BankAccount {
+    int Account_Number;
+    String Name;
+    int Balance;
+}
+class CreateAccount inherit Bankacc
+class Bank{
+    
+}
